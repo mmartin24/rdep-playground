d79678e8a33b10ce2698d697460aeb4aadcf8e3f
Manuel Martin <manuel.martin@suse.com>
2023-10-24 11:34:23 +0200

    trying bringing back wait on helm install ep

--- ep_install_on_rd.sh
+++ ep_install_on_rd.sh
@@ -32,12 +32,12 @@ echo -e '\n ---------- INSTALLING EPINIO ------------ \n'
 helm repo add epinio https://epinio.github.io/helm-charts 
 #MYEPINIODOMAIN=`kubectl get svc -n kube-system traefik | awk '{print $4}' | tail --lines=+2` 
 MYEPINIODOMAIN='127.0.0.1'
-helm upgrade --install epinio -n epinio --create-namespace epinio/epinio \
+helm --wait upgrade --install epinio -n epinio --create-namespace epinio/epinio \
     --set global.domain=${MYEPINIODOMAIN}.nip.io \
     --set server.disableTracking="true" 
 
 #kubectl wait --for=condition=Ready --timeout=180s pods --all -n epinio
-sleep 60
+#sleep 60
 
 ## Check it can login
 echo -e '\n ---------- CHECKING EPINIO LOGIN ------------ \n'

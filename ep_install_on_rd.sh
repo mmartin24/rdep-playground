c3a315896e4112b5b626805ac0da40ec8cdf2705
Manuel Martin <manuel.martin@suse.com>
2023-10-26 10:21:18 +0200

    extending timeout epinio. bringinng k check for condition ready

--- ep_install_on_rd.sh
+++ ep_install_on_rd.sh
@@ -32,11 +32,11 @@ echo -e '\n ---------- INSTALLING EPINIO ------------ \n'
 helm repo add epinio https://epinio.github.io/helm-charts 
 #MYEPINIODOMAIN=`kubectl get svc -n kube-system traefik | awk '{print $4}' | tail --lines=+2` 
 MYEPINIODOMAIN='127.0.0.1'
-helm upgrade --install --wait --timeout=120s epinio -n epinio --create-namespace epinio/epinio \
+helm upgrade --install --wait --timeout=480s epinio -n epinio --create-namespace epinio/epinio \
     --set global.domain=${MYEPINIODOMAIN}.nip.io \
     --set server.disableTracking="true" 
 
-#kubectl wait --for=condition=Ready --timeout=180s pods --all -n epinio
+kubectl wait --for=condition=Ready --timeout=180s pods --all -n epinio
 #sleep 60
 
 ## Check it can login
